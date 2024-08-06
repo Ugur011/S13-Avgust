d6ceb6af600d010ad7b7d7ff02b81ac45f40d6a1
UgurAlizade <162479967+UgurAlizade@users.noreply.github.com>
2024-08-06 16:48:17 +0400

    item javascript

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let read = document.querySelector(".read-more");
 let more = document.querySelector(".more-text")
 let selectTime = document.querySelector(".select-time")
+let dates = document.querySelectorAll(".item");
 
 let toggle = false
 
@@ -12,4 +13,13 @@ read.addEventListener('click', function () {
         more.style.display = "none";
         selectTime.style.marginTop = "18px"
     }
+});
+
+dates.forEach(element => {
+    element.addEventListener('click', function() {
+        dates.forEach(date => {
+            date.style.backgroundColor = "rgb(75, 15, 21)"
+        });
+        this.style.backgroundColor = 'rgb(150,35,45)';
+    });
 });
