743092ba4e3804e1fc50404e45daa524a8365b84
UgurAlizade <162479967+UgurAlizade@users.noreply.github.com>
2024-08-06 23:34:59 +0400

    2ci hisse JS

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let read = document.querySelector(".read-more");
-let more = document.querySelector(".more-text")
-let selectTime = document.querySelector(".select-time")
+let more = document.querySelector(".more-text");
+let selectTime = document.querySelector(".select-time");
 let dates = document.querySelectorAll(".item");
+let seats = document.querySelectorAll('.fa-couch');
+let number = 0;
+let price = document.querySelector('.price');
+let cost = 0;
 
 let toggle = false
 
@@ -21,5 +25,52 @@ dates.forEach(element => {
             date.style.backgroundColor = "rgb(75, 15, 21)"
         });
         this.style.backgroundColor = 'rgb(150,35,45)';
+    });
+});
+
+function randomNumber() {
+    let set = new Set();
+    while (set.size < 14) {
+        set.add(Math.floor(Math.random() * 53));
+    }
+    return Array.from(set);
+}
+
+dates.forEach(element => {
+    element.addEventListener('click', function() {
+        seats.forEach(element => {
+            element.style.color = 'white';
+        });
+
+        let arr = randomNumber();
+        seats[arr[0]].style.color = 'red';
+        seats[arr[1]].style.color = 'red';
+        seats[arr[2]].style.color = 'red';
+        seats[arr[3]].style.color = 'red';
+        seats[arr[4]].style.color = 'red';
+        seats[arr[5]].style.color = 'red';
+        seats[arr[6]].style.color = 'red';
+        seats[arr[7]].style.color = 'red';
+        seats[arr[8]].style.color = 'red';
+        seats[arr[9]].style.color = 'red';
+        seats[arr[10]].style.color = 'red';
+        seats[arr[11]].style.color = 'red';
+        seats[arr[12]].style.color = 'red';
+        seats[arr[13]].style.color = 'red';
+    })
+});
+
+seats.forEach(element => {
+    element.addEventListener('click', function() {
+        let isDateSelected = Array.from(dates).some(date => date.style.backgroundColor === 'rgb(150, 35, 45)');
+        if (isDateSelected && element.style.color === 'white') {
+            element.style.color = 'rgb(47, 156, 207)';
+            cost = cost+40;
+            price.innerText = "Total " + "$" + cost;
+        } else if (element.style.color === 'rgb(47, 156, 207)') {
+            element.style.color = 'white';
+            cost = cost-40;
+            price.innerText = "Total " + "$" + cost;
+        }
     });
 });
