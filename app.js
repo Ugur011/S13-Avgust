63ec0ee3178220b0312b515200f30cd0c0ae6c6b
Ugur Yusifzade <158311923+Ugur011@users.noreply.github.com>
2024-08-07 14:15:14 -0700

    kicik elave

--- app.js
+++ app.js
@@ -161,20 +161,20 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 
@@ -189,20 +189,20 @@ var btn1 = document.getElementById("myBtn1");
 var span1 = document.getElementsByClassName("close1")[0];
 
 // When the user clicks the button, open the modal 
-btn1.onclick = function() {
-  modal1.style.display = "block";
+btn1.onclick = function () {
+    modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  modal1.style.display = "none";
+span1.onclick = function () {
+    modal1.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1) {
-    modal1.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
 }
 window.onload = function () {
     let purchaseBtn = document.querySelector('.purchase--btn');
